3ea87a944ae587c2d9dad09b06f624f75bff9021
ahohanov <hohanov@gmail.com>
2024-01-30 11:32:37 +0300

    vuetify install

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,20 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+// Vuetify
+import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+const vuetify = createVuetify({
+  display: {
+    mobileBreakpoint: 'md',
+  },
+  components,
+  directives,
+})
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +29,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
